e90545013edb11a9997e11d9cce6511a8ab23d69
Arkadiusz Kleban <arkadiusz.kleban@gmail.com>
2019-07-10 22:12:41 +0200

    Implementacja warstwy usług

---
+++ src/main/java/pl/sda/service/OrderService.java
@@ -0,0 +1,5 @@
+package pl.sda.service;
+
+public interface OrderService {
+    void processOrder(String productId, int count);
+}
---
+++ src/main/java/pl/sda/service/impl/OrderServiceImpl.java
@@ -0,0 +1,33 @@
+package pl.sda.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.sda.domain.Product;
+import pl.sda.domain.repository.ProductRepository;
+import pl.sda.service.OrderService;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void processOrder(String productId, long quantity) {
+        Product productById = productRepository.getProductById(productId);
+
+        if(productById.getUnitsInStock() < quantity){
+            throw new IllegalArgumentException("Zbyt mało towaru. Obecna liczba sztuk w magazynie "+ productById.getUnitsInStock());
+        }
+
+        productById.setUnitsInStock(productById.getUnitsInStock() - quantity);
+    }
+
+    @Override
+    public void processOrder(String productId, int count) {
+
+    }
+}
+
+
+
+
